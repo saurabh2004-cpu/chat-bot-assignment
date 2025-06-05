69993a7fec73195be9876b3bc6ed4751dcd8ebf3
Your Name <saurabhbodakhe2004@gmail.com>
2025-06-05 11:46:51 +0530

    resolved build error

--- src/components/home-component.tsx
+++ src/components/home-component.tsx
@@ -33,7 +33,6 @@ export default function HomeComponent() {
         </div>
       </div>
 
-      //floating button
       <button
         onClick={toggleChat}
         className="fixed bottom-6 right-6 w-14 h-14 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center shadow-lg transition-all duration-200 z-50"
